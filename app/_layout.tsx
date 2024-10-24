7c46df8d308196a6d0b68a18841bb909173061e9
Rajdeep Barman <deepbn9@gmail.com>
2024-10-24 11:43:37 +0530

    feat: added gluestackui configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
-
+import { Client } from 'react-native-appwrite';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import 'react-native-reanimated';
+import "@/global.css";
+
+let client: Client;
+
+client = new Client();
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('67174863000775f8f2b8')   // Your Project ID
+  .setPlatform('com.dev.rajdeep.gluestackdemo');   // Your package name / bundle identifier
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -27,11 +37,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <GluestackUIProvider mode="light">
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
